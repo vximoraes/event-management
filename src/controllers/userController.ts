6628fa84ee18458d158965a33f61487424726c4d
vinimoraes <viniciusmoraesvha@gmail.com>
2025-02-06 18:25:05 -0400

    feat: implement user table creation and enhance user creation error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,16 @@
 import { validateUser } from '../validations/userValidation'
-import { insertUser } from '../services/userService'
+import { createUserTableDb, createUserDb } from '../services/userService'
 import { User } from '../models/userModel'
+
+export function createUserTable() {
+    try {
+        createUserTableDb()
+    } catch (error) {
+        console.log(`Erro ao criar a tabela: ${error}`)
+    }
+
+    console.log(`Tabela criada com sucesso!`)
+}
 
 export function createUser(name: string, email: string, password: string) {
     const user: User = { 
@@ -9,12 +19,23 @@ export function createUser(name: string, email: string, password: string) {
         password 
     }
 
+    // Valida o usuário
     const validation = validateUser(user)
 
+    // Se a validação falhar, exibe os erros e retorna
     if (!validation.success) {
-        console.log("Erro de validação:", validation.error.errors)
-        return 
+        console.log("Erros de validação:")
+        validation.error.errors.forEach((err) => {
+            console.log(`- ${err.path.join(".")}: ${err.message}`)
+        })
+        return
     }
 
-    insertUser(user)
-} 
+    // Tenta criar o usuário no banco de dados
+    try {
+        createUserDb(user);
+        console.log("Usuário criado com sucesso!");
+    } catch (error) {
+        console.log(`Erro ao criar usuário: ${error instanceof Error ? error.message : "Erro desconhecido."}`);
+    }
+}
