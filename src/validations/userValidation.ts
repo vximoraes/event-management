ccac16c118c7d647cd620cb274a4942a9e2b3a1f
vinimoraes <viniciusmoraesvha@gmail.com>
2025-02-06 18:25:23 -0400

    refactor: rename user validation fields for consistency

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -3,9 +3,9 @@ import { User } from "../models/userModel"
 
 // Define o schema de validação para um usuário
 const userSchema = z.object({
-    nome : z.string().min(1, "Nome é obrigatório"), // Valida o nome (não vazio)
+    name : z.string().min(1, "Nome é obrigatório"), // Valida o nome (não vazio)
     email: z.string().email("E-mail inválido"),    // Valida o formato do e-mail
-    senha: z
+    password: z
         .string()
         .min(8, "A senha deve ter pelo menos 8 caracteres") // Valida a senha com mínimo de 8 caracteres
         .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula") // Valida se possui letra maiúscula
