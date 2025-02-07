70368971bf4f0591069c4c3c988e4fde21522086
vinimoraes <viniciusmoraesvha@gmail.com>
2025-02-07 18:01:09 -0400

    feat: enhance user management functions with async operations and improved error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,41 +1,61 @@
 import { validateUser } from '../validations/userValidation'
-import { createUserTableDb, createUserDb } from '../services/userService'
+import { createUserDb, createUserTableDb, listAllUsersDb } from '../services/userService'
 import { User } from '../models/userModel'
 
-export function createUserTable() {
+// Funcionando
+export async function createUserTable() {
     try {
-        createUserTableDb()
-    } catch (error) {
-        console.log(`Erro ao criar a tabela: ${error}`)
-    }
+        const createdTable = await createUserTableDb()
 
-    console.log(`Tabela criada com sucesso!`)
+        if (createdTable) {
+            console.log(`\nTabela users criada com sucesso!`)
+        }
+    } catch (error) {
+        console.log(`\nErro ao criar a tabela users: ${error}`)
+    }
 }
 
-export function createUser(name: string, email: string, password: string) {
-    const user: User = { 
-        name, 
-        email, 
-        password 
+// Funcionando
+export async function createUser(name: string, email: string, password: string) {
+    const user: User = {
+        name,
+        email,
+        password
     }
 
-    // Valida o usuário
     const validation = validateUser(user)
 
-    // Se a validação falhar, exibe os erros e retorna
     if (!validation.success) {
-        console.log("Erros de validação:")
+        console.log("\nErros de validação ao inserir usuário:")
         validation.error.errors.forEach((err) => {
             console.log(`- ${err.path.join(".")}: ${err.message}`)
         })
         return
     }
 
-    // Tenta criar o usuário no banco de dados
     try {
-        createUserDb(user);
-        console.log("Usuário criado com sucesso!");
+        const createdUser = await createUserDb(user)
+
+        if (createdUser) {
+            console.log(`\nUsuário inserido com sucesso!`)
+        }
     } catch (error) {
-        console.log(`Erro ao criar usuário: ${error instanceof Error ? error.message : "Erro desconhecido."}`);
+        console.log(`\nErro ao criar usuário: ${error}}`)
+    }
+}
+
+// Funcionando
+export async function listAllUsers() {
+    try {
+        const users = await listAllUsersDb()  
+
+        if (users && users.length > 0) {
+            console.log(`\nUsuários cadastrados:`)
+            console.log(users)  
+        } else {
+            console.log('Nenhum usuário encontrado.')
+        }
+    } catch (error) {
+        console.error(error) 
     }
 }
