51c889cd98376ba5c9a02230f49c411560458478
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-08 21:00:01 -0400

    refactor: update index.ts to integrate event controller functions and remove unused user functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import { createUser, createUserTable, listAllUsers, listUserById } from "./controllers/userController";
+import { createEvent, createEventTable, deleteEvent, listAllEvents, listEvent, updateEvent } from "./controllers/eventController";
+import { createUser, createUserTable, deleteUser, listAllUsers, listUser, updateUser } from "./controllers/userController";
 
 //createUserTable()
 //createUser("Vini", "vinicius@gmail.com", "12345678@Senha")
 //listAllUsers()
-listUserById(7)
+//listUser(1)
+//updateUser(4, "Vinícius Moraes", "viniciusmoraesvha@gmail.com", "#Senha1800VX")
+//deleteUser(7)
+//listAllUsers()
+
+//createEventTable()
+//createEvent("Meu evento", new Date, 10)
+//listAllEvents()
+//listEvent(12)
+//updateEvent(16, "Evento Alterado", new Date, 10)
+deleteEvent(5)
