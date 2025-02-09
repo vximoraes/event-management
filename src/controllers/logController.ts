20ec25632b5650e7e12bcbc74713962cc8252c47
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-08 23:24:23 -0400

    feat: implement createLogsTable function to initialize logs table and handle errors

---
+++ src/controllers/logController.ts
@@ -0,0 +1,15 @@
+import { createLogsTableDb } from "../services/logService";
+import { getCurrentTime } from "../utils/loggerUtils";
+
+// Função para criar a tabela de logs
+export async function createLogsTable() {
+    try {
+        const createdTable = await createLogsTableDb();
+
+        if (createdTable) {
+            console.log(`${getCurrentTime()} - Tabela logs criada com sucesso!`);
+        }
+    } catch (error) {
+        console.error(`${getCurrentTime()} - Erro ao criar a tabela logs: ${error}`);
+    }
+}
