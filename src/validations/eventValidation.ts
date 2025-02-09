0b38fb7e5d6a1d752e3dcbc6bfce22834adb6068
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-09 19:52:10 -0400

    chore: remove commented validation schema for cleaner code

--- src/validations/eventValidation.ts
+++ src/validations/eventValidation.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";  
 import { Event } from "../models/eventModel"
 
-// Define o schema de validação para um evento  
 export const eventSchema = z.object({  
-    name: z.string().min(1, "Nome é obrigatório"), // Valida a data (não vazio) 
-    user_id: z.number().int().positive("O ID do usuário deve ser um número positivo") // Valida o ID do usuário como um número inteiro positivo  
+    name: z.string().min(1, "Nome é obrigatório"),
+    user_id: z.number().int().positive("O ID do usuário deve ser um número positivo") 
 })
 
-// Função para validar o evento  
 export function validateEvent(event: Event) {  
     return eventSchema.safeParse(event)
 } 
