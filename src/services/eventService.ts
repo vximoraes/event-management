861b48f6fe200a236ff552d7607ca7d7d4cada91
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-08 20:58:44 -0400

    refactor: update event service functions to return promises and improve error handling

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,18 +1,14 @@
-import { Event } from './../models/eventModel'
 import sqlite3 from "sqlite3"
+import { Event } from './../models/eventModel'
+import { formatEventDate } from "../utils/dateUtils"
 
-const db = new sqlite3.Database('./data/eventos.db')
+const db = new sqlite3.Database('./data/database.db')
 
 // Ativa a verificação de chaves estrangeiras
-db.run('PRAGMA foreign_keys = ON', (error) => {
-    if (error) {
-        console.log('Erro ao ativar chaves estrangeiras:', error)
-    } else {
-        console.log('Chaves estrangeiras ativadas com sucesso!')
-    }
-})
+db.run('PRAGMA foreign_keys = ON')
 
-export function createEventTable() {
+// Funcionando
+export function createEventTableDb(): Promise<boolean> {
     const query = `
         CREATE TABLE IF NOT EXISTS events (
             id         INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,90 +19,108 @@ export function createEventTable() {
         )
     `
 
-    db.run(query, (error) => {
-        if (error) {
-            console.log(`Erro ao criar a tabela: ${error}`)
-        } else {
-            console.log(`Tabela criada com sucesso!`)
-        }
+    return new Promise((resolve, reject) => {
+        db.run(query, (error) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(true)
+            }
+        })
     })
 }
 
-export function insertEvent(event: Event): void {
+// Funcionando
+export function createEventDb(event: Event): Promise<any> {
     const query = `
         INSERT INTO events (name, date, user_id)
         VALUES (?, ?, ?)
     `
 
-    db.run(query, [event.name, event.date, event.user.id], function (error) {
-        if (error) {
-            console.log(`Erro ao inserir evento: ${error}`)
-        } else {
-            console.log(`Evento ${this.lastID} inserido com sucesso!`)
-        }
+    return new Promise((resolve, reject) => {
+        const formattedDate = formatEventDate(event.date)
+        db.run(query, [event.name, formattedDate, event.user_id], function (error) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(true)
+            }
+        })
     })
 }
 
-export function listAllEvents() {
+// Funcionando
+export function listAllEventsDb(): Promise<any[]> {
     const query = `
         SELECT * FROM events
     `
 
-    db.all(query, (error, linhas) => {
-        if (error) {
-            console.log(`Erro ao listar eventos: ${error}`)
-        } else {
-            console.log(linhas)
-        }
+    return new Promise((resolve, reject) => {
+        db.all(query, (error, rows) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(rows)
+        })
     })
 }
 
-export function listEventById(id: number) {
+// Funcionando
+export function listEventDb(id: number): Promise<any> {
     const query = `
         SELECT * FROM events WHERE id = ?
     `
 
-    db.get(query, [id], (error, linha) => {
-        if (error) {
-            console.log(`Erro ao listar evento: ${error}`)
-        } else if (linha) {
-            console.log(linha)
-        } else {
-            console.log(`Nenhum evento encontrado pelo id ${id}`)
-        }
+    return new Promise((resolve, reject) => {
+        db.get(query, [id], (error, row) => {
+            if (error) {
+                reject(error)
+            } else if (row) {
+                resolve(row)
+            } else {
+                resolve(null)
+            }
+        })
     })
 }
 
-export function updateEventById(event: Event) {
+// Funcionando
+export function updateEventDb(event: Event): Promise<any> {
     const query = `
         UPDATE events 
         SET name = ?, date = ?, user_id = ?
         WHERE id = ?
     `
 
-    db.run(query, [event.name, event.date, event.user.id, event.id], function (error) {
-        if (error) {
-            console.log(`Erro ao alterar evento: ${error}`)
-        } else if (this.changes === 0) {
-            console.log(`Nenhum evento encontrado pelo id ${event.id}`) 
-        } else {
-            console.log(`Evento ${event.id} alterado com sucesso!`)
-        }
+    return new Promise((resolve, reject) => {
+        const formattedDate = formatEventDate(event.date)
+        db.run(query, [event.name, formattedDate, event.user_id, event.id], function (error) {
+            if (error) {
+                reject(error)
+            } else if (this.changes === 0) {
+                resolve(false)
+            } else {
+                resolve(true)
+            }
+        })
     })
 }
 
-export function deleteEvent(id: number) {
+// Funcionando
+export function deleteEventDb(id: number): Promise<any> {
     const query = `
         DELETE FROM events WHERE id = ?
     `
 
-    db.run(query, [id], function (erro) {
-        if (erro) {
-            console.log(`Erro ao deletar evento: ${erro}`)
-        } if (this.changes === 0) {
-            console.log(`Nenhum evento encontrado pelo id ${id}`)
-        } else {
-            console.log(`Evento ${id} deletado com sucesso!`)
-        }
+    return new Promise((resolve, reject) => {
+        db.run(query, [id], function (error) {
+            if (error) {
+                reject(error)
+            } else if (this.changes === 0) {
+                resolve(false)
+            } else {
+                resolve(true)
+            }
+        })
     })
 }
